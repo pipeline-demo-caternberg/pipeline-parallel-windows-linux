61336aaf5185c863c19f283309ee39280008ef58
andreas <caternberg@gmx.de>
2022-11-30 15:14:27 +0100

    add

---
+++ JenkinsfileWin.groovy
@@ -0,0 +1,55 @@
+pipeline {
+    agent none
+
+    stages {
+        stage("build and deploy on Windows and Linux") {
+            parallel {
+                stage("windows") {
+                    agent {
+                        kubernetes {
+                            yamlFile 'pods/win.yaml'
+                            defaultContainer 'shell'
+                        }
+                    }
+                    stages {
+                        stage("build") {
+                            steps {
+                                echo "build WINDOWS"
+                               // run powershell command here
+                               powershell 'Get-ChildItem Env: | Sort Name'
+                            }
+                        }
+                        stage("deploy") {
+                            steps {
+                                echo "deploy threadA"
+                            }
+                        }
+                    }
+                }
+
+                stage("windows2") {
+                    agent {
+                        kubernetes {
+                            yamlFile 'pods/win.yaml'
+                            defaultContainer 'shell'
+                        }
+                    }
+                    stages {
+                        stage("build") {
+                            steps {
+                                echo "build WINDOWS"
+                                // run powershell command here
+                                powershell 'Get-ChildItem Env: | Sort Name'
+                            }
+                        }
+                        stage("deploy") {
+                            steps {
+                                echo "deploy threadA"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
