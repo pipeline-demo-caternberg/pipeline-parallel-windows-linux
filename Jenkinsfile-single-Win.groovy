09272b3a44695d10d97ca6653270dbcfb32eee83
andreas <caternberg@gmx.de>
2022-11-30 17:49:06 +0100

    add

---
+++ Jenkinsfile-single-Win.groovy
@@ -0,0 +1,24 @@
+podTemplate(yaml: '''
+      apiVersion: v1
+      kind: Pod
+      spec:
+        containers:
+        - name: jnlp
+          image: jenkins/inbound-agent:jdk11-windowsservercore-1809
+        - name: shell
+          image: mcr.microsoft.com/powershell:preview-windowsservercore-1809
+          command:
+          - powershell
+          args:
+          - Start-Sleep
+          - 999999
+        nodeSelector:
+          beta.kubernetes.io/os: windows
+      ''') {
+    node(POD_LABEL) {
+        container('shell') {
+            // run powershell command here
+            powershell 'Get-ChildItem Env: | Sort Name'
+        }
+    }
+}
