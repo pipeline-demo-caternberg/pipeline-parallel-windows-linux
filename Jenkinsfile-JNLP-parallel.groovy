8b667fd3eeec35fe3d2acc5184d5eac7218b56ef
andreas <caternberg@gmx.de>
2022-12-18 14:10:43 +0100

    add

---
+++ Jenkinsfile-JNLP-parallel.groovy
@@ -0,0 +1,64 @@
+pipeline {
+    agent none
+    stages {
+        stage("JNLP") {
+            parallel {
+                stage("stage-jnlp1") {
+                    options {
+                        skipDefaultCheckout(true)
+                    }
+                    agent {
+                        kubernetes {
+                            cloud "myk8s"
+                            yamlFile 'pods/jnlp.yaml'
+                            defaultContainer 'jnlp'
+                        }
+                    }
+                    steps {
+                        echo "steps WINDOWS"
+                        powershell 'Get-ChildItem Env: | Sort Name'
+                        powershell 'Write-Output "Hello JNLP1"'
+                        powershell 'Get-ChildItem -Force'
+                        /* powershell '''for ($i = 0; $i -lt 5; $i++){
+                                             "$i";
+                                             ls -Force;
+                                             Get-Date;
+                                             Start-Sleep -Seconds 5;
+                                             Write-Host "ECHO FROM WINDOWS"
+                                         }'''
+
+                         */
+                    }
+                }
+                stage("stage-jnlp2") {
+                    options {
+                        skipDefaultCheckout(true)
+                    }
+                    agent {
+                        kubernetes {
+                            cloud "myk8s"
+                            yamlFile 'pods/jnlp.yaml'
+                            defaultContainer 'jnlp'
+                        }
+                    }
+                    steps {
+                        echo "steps WINDOWS"
+                        powershell 'Get-ChildItem Env: | Sort Name'
+                        powershell 'Write-Output "Hello JNLP2"'
+                        powershell 'Get-ChildItem -Force'
+                        /* powershell '''for ($i = 0; $i -lt 5; $i++){
+                                             "$i";
+                                             ls -Force;
+                                             Get-Date;
+                                             Start-Sleep -Seconds 5;
+                                             Write-Host "ECHO FROM WINDOWS"
+                                         }'''
+
+                         */
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ benchmark.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+# For loop 5 times
+for i in {1..5}
+do
+
+cat <<EOF | kubectl delete -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: windows-pod-${i}
+spec:
+  containers:
+  - name: windows
+    image: mcr.microsoft.com/powershell:preview-windowsservercore-1809
+    #      resources:
+    #        requests:
+    #          memory: "8Gi"
+    #          cpu: "2"
+    #          ephemeral-storage: "1Gi"
+    #        limits:
+    #          memory: "8Gi"
+    #          cpu: "4"
+    #          ephemeral-storage: "2Gi"
+    imagePullPolicy: IfNotPresent
+    command:
+      - powershell
+    args:
+      - "Start-Sleep"
+      - '999999'
+  nodeSelector:
+    kubernetes.io/os: windows
+EOF
+
+done
+
+## Put all cust_func in the background and bash
+## would wait until those are completed
+## before displaying all done message
+wait
+echo "All done"
