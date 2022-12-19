8712526491c087c90b04b012c03f8f7794111ddb
andreas <caternberg@gmx.de>
2022-12-19 15:10:28 +0100

    add, rename

---
+++ helperscripts.groovy
@@ -0,0 +1,15 @@
+//Delete build queue
+import hudson.model.*
+import jenkins.model.Jenkins
+
+def q = Jenkins.instance.queue
+
+q.items.findAll { it.task.name.startsWith('my') }.each { q.cancel(it.task) }
+
+//delete all idle nodes
+for (aSlave in hudson.model.Hudson.instance.slaves) {
+    if (aSlave.getComputer().isOffline()) {
+        aSlave.getComputer().setTemporarilyOffline(true,null);
+        aSlave.getComputer().doDoDelete();
+    }
+}
