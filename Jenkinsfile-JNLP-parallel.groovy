ac9f571834575fddc37594b8ccf355c812a3861e
andreas <caternberg@gmx.de>
2022-12-19 08:24:46 +0100

    add stages

--- Jenkinsfile-JNLP-parallel.groovy
+++ Jenkinsfile-JNLP-parallel.groovy
@@ -55,6 +55,84 @@ pipeline {
                          */
                     }
                 }
+                stage("stage-jnlp3") {
+                    options {
+                        skipDefaultCheckout(true)
+                    }
+                    agent {
+                        kubernetes {
+                            cloud "myk8s"
+                            yamlFile 'pods/jnlp.yaml'
+                            defaultContainer 'jnlp'
+                        }
+                    }
+                    steps {
+                        powershell 'Get-ChildItem Env: | Sort Name'
+                        powershell 'Write-Output "Hello JNLP3"'
+                        powershell 'Get-ChildItem -Force'
+                        /* powershell '''for ($i = 0; $i -lt 5; $i++){
+                                             "$i";
+                                             ls -Force;
+                                             Get-Date;
+                                             Start-Sleep -Seconds 5;
+                                             Write-Host "ECHO FROM WINDOWS"
+                                         }'''
+
+                         */
+                    }
+                }
+                stage("stage-jnlp4") {
+                    options {
+                        skipDefaultCheckout(true)
+                    }
+                    agent {
+                        kubernetes {
+                            cloud "myk8s"
+                            yamlFile 'pods/jnlp.yaml'
+                            defaultContainer 'jnlp'
+                        }
+                    }
+                    steps {
+                        powershell 'Get-ChildItem Env: | Sort Name'
+                        powershell 'Write-Output "Hello JNLP4"'
+                        powershell 'Get-ChildItem -Force'
+                        /* powershell '''for ($i = 0; $i -lt 5; $i++){
+                                             "$i";
+                                             ls -Force;
+                                             Get-Date;
+                                             Start-Sleep -Seconds 5;
+                                             Write-Host "ECHO FROM WINDOWS"
+                                         }'''
+
+                         */
+                    }
+                }
+                stage("stage-jnlp5") {
+                    options {
+                        skipDefaultCheckout(true)
+                    }
+                    agent {
+                        kubernetes {
+                            cloud "myk8s"
+                            yamlFile 'pods/jnlp.yaml'
+                            defaultContainer 'jnlp'
+                        }
+                    }
+                    steps {
+                        powershell 'Get-ChildItem Env: | Sort Name'
+                        powershell 'Write-Output "Hello JNLP5"'
+                        powershell 'Get-ChildItem -Force'
+                        /* powershell '''for ($i = 0; $i -lt 5; $i++){
+                                             "$i";
+                                             ls -Force;
+                                             Get-Date;
+                                             Start-Sleep -Seconds 5;
+                                             Write-Host "ECHO FROM WINDOWS"
+                                         }'''
+
+                         */
+                    }
+                }
             }
         }
     }
